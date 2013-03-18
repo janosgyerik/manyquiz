dbca662eca00357511c546fde6dbd45e12566b65
Janos Gyerik <janos@axiom>
2013-03-18 23:34:09 +0100

    added export script

---
+++ django-quiz/quiz/management/commands/export_questions.py
@@ -0,0 +1,36 @@
+from optparse import make_option
+
+from django.core.management.base import BaseCommand
+
+from quiz.models import Question
+
+END_MARKER = 'END'
+
+
+def export_questions(limit=10):
+    query = Question.objects.all()
+    if limit:
+        query = query[:limit]
+    for question in query:
+        print question.text
+        for answer in question.answer_set.all().order_by('-is_correct'):
+            print answer.text
+        print question.explanation
+        print question.level.level
+        print question.category
+        print END_MARKER
+
+
+class Command(BaseCommand):
+    help = 'Export questions in plain text format'
+
+    option_list = BaseCommand.option_list + (
+            make_option('--limit', '-l', type=int,
+                help='Max number of questions to export'),
+            )
+
+    def handle(self, *args, **options):
+        export_questions(options['limit'])
+
+
+# eof
