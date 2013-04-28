24ba81dbdd48739372eba18424e372d8e9d10f20
Janos Gyerik <janos@axiom>
2013-04-28 18:47:46 +0200

    moved ads to dedicated ads.xml, included by layouts, GONE by default

--- library/src/com/manyquiz/QuizBaseActivity.java
+++ library/src/com/manyquiz/QuizBaseActivity.java
@@ -13,28 +13,22 @@ abstract class QuizBaseActivity extends Activity{
 
 	private static final String ADMOB_UNIT_ID = "a14b9eac992329e";
 
-	private AdView googleAdMobAds;
+	private AdView googleAdMobView;
 
 	protected void checkAndSetupForLiteVersion() {
 		if (((QuizApplication)this.getApplication()).isLiteVersion()) {
 			findViewById(R.id.lite_watermark).setVisibility(View.VISIBLE);
 			setupAds();
-			showAds();
 		}
 	}
 
 	protected void setupAds() {
-		googleAdMobAds = new AdView(this, AdSize.BANNER, ADMOB_UNIT_ID);
-		googleAdMobAds.setBackgroundColor(Color.BLACK);
-
-		LinearLayout layout = (LinearLayout)findViewById(R.id.googleAdMobAds);
-		layout.addView(googleAdMobAds);
-
-		googleAdMobAds.loadAd(new AdRequest());
-		googleAdMobAds.setVisibility(View.GONE);
-	}
-
-	protected void showAds() {
-		googleAdMobAds.setVisibility(View.VISIBLE);
+		googleAdMobView = new AdView(this, AdSize.BANNER, ADMOB_UNIT_ID);
+		googleAdMobView.setBackgroundColor(Color.BLACK);
+		googleAdMobView.loadAd(new AdRequest());
+		
+		LinearLayout adsLayout = (LinearLayout)findViewById(R.id.ads);
+		adsLayout.addView(googleAdMobView);
+		adsLayout.setVisibility(View.VISIBLE);
 	}
 }
