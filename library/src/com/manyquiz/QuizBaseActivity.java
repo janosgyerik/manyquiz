fc1f0eb7a052641092a0540bcef341d391924cd3
Janos Gyerik <janos@axiom>
2013-04-28 20:00:30 +0200

    configure the ad using xml

--- library/src/com/manyquiz/QuizBaseActivity.java
+++ library/src/com/manyquiz/QuizBaseActivity.java
@@ -3,17 +3,11 @@ package com.manyquiz;
 import android.app.Activity;
 import android.graphics.Color;
 import android.view.View;
-import android.widget.LinearLayout;
 
 import com.google.ads.AdRequest;
-import com.google.ads.AdSize;
 import com.google.ads.AdView;
 
 abstract class QuizBaseActivity extends Activity{
-
-	private static final String ADMOB_UNIT_ID = "a14b9eac992329e";
-
-	private AdView googleAdMobView;
 
 	protected void checkAndSetupForLiteVersion() {
 		if (((QuizApplication)this.getApplication()).isLiteVersion()) {
@@ -23,12 +17,10 @@ abstract class QuizBaseActivity extends Activity{
 	}
 
 	protected void setupAds() {
-		googleAdMobView = new AdView(this, AdSize.BANNER, ADMOB_UNIT_ID);
+		AdView googleAdMobView = (AdView)this.findViewById(R.id.google_admob);
 		googleAdMobView.setBackgroundColor(Color.BLACK);
 		googleAdMobView.loadAd(new AdRequest());
 		
-		LinearLayout adsLayout = (LinearLayout)findViewById(R.id.ads);
-		adsLayout.addView(googleAdMobView);
-		adsLayout.setVisibility(View.VISIBLE);
+		findViewById(R.id.ads).setVisibility(View.VISIBLE);
 	}
 }
