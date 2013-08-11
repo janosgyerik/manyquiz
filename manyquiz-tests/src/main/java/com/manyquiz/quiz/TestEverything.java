1f1c1ea17328b51f0566fef7fbadcad1d4f561ce
Janos Gyerik <janos@xw8400>
2013-08-11 17:20:56 +0200

    reformatted unit test codes

--- manyquiz-tests/src/main/java/com/manyquiz/quiz/TestEverything.java
+++ manyquiz-tests/src/main/java/com/manyquiz/quiz/TestEverything.java
@@ -4,6 +4,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ ScoreInTheEndTest.class, ScoreAsYouGoTest.class, SuddenDeathTest.class })
+@Suite.SuiteClasses({ScoreInTheEndTest.class, ScoreAsYouGoTest.class, SuddenDeathTest.class})
 public class TestEverything {
 }
