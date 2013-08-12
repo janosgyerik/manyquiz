228166eed45eea125f81dffc0d1f975b12648419
Janos Gyerik <info@janosgyerik.com>
2013-08-12 23:51:13 +0200

    added --delete-level flag

--- django-quiz/quiz/management/commands/update.py
+++ django-quiz/quiz/management/commands/update.py
@@ -9,16 +9,22 @@ class Command(BaseCommand):
     help = 'Update quiz data'
 
     option_list = BaseCommand.option_list + (
-            make_option('--set-level', type='int', dest='level',
+            make_option('--set-level', type='int',
                 help='Set level on all questions'),
+            make_option('--delete-level',
+                help='Delete specified level'),
             make_option('--delete-category',
                 help='Delete specified category'),
             )
 
     def handle(self, *args, **options):
-        if options['level']:
-            level = Level.objects.get(level=options['level'])
+        if options['set_level']:
+            level = Level.objects.get(level=options['set_level'])
             Question.objects.update(level=level)
+
+        if options['delete_level']:
+            level = Level.objects.get(level=options['delete_level'])
+            Question.objects.filter(level=level).delete()
 
         if options['delete_category']:
             category = options['delete_category']
