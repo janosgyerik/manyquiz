5af3db5fad6acadd8dc775217214832b13db01cb
Janos Gyerik <janos@xw8400>
2013-08-17 20:40:50 +0200

    organized imports

--- manyquiz/src/com/manyquiz/activity/ContactActivity.java
+++ manyquiz/src/com/manyquiz/activity/ContactActivity.java
@@ -1,7 +1,6 @@
 package com.manyquiz.activity;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
@@ -11,7 +10,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.EditText;
 import android.widget.RadioGroup;
-import android.widget.Toast;
 
 import com.manyquiz.R;
 import com.manyquiz.tools.EmailTools;
--- manyquiz/src/com/manyquiz/activity/ResultsActivity.java
+++ manyquiz/src/com/manyquiz/activity/ResultsActivity.java
@@ -1,12 +1,10 @@
 package com.manyquiz.activity;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.manyquiz.R;
 import com.manyquiz.quiz.model.IQuestion;
