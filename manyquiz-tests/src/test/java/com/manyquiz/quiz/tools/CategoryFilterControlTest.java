70c1a059f6a427665e3d6ce2245429610bf3c2b8
Janos Gyerik <info@janosgyerik.com>
2013-10-31 02:19:49 +0100

    renamed test class to match the one under test

--- manyquiz-tests/src/test/java/com/manyquiz/quiz/tools/CategoryFilterControlTest.java
+++ manyquiz-tests/src/test/java/com/manyquiz/quiz/tools/CategoryFilterControlTest.java
@@ -15,7 +15,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-public class CategoryControlTest {
+public class CategoryFilterControlTest {
 
     @Test
     public void testCategoriesEnabledByDefault() {
