8c7853ba7cf4eb20d049f6103d7b8bb39bb01b27
Janos Gyerik <info@janosgyerik.com>
2013-11-04 01:33:12 +0100

    added fade-out option in settings

--- manyquiz/src/com/manyquiz/activity/SettingsActivity.java
+++ manyquiz/src/com/manyquiz/activity/SettingsActivity.java
@@ -1,21 +1,11 @@
 package com.manyquiz.activity;
 
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
-import android.preference.Preference;
 import android.preference.PreferenceActivity;
-import android.preference.PreferenceGroup;
-import android.preference.PreferenceManager;
-import android.util.Log;
 
 import com.manyquiz.R;
 
-public class SettingsActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener {
-
-    private static final String TAG = SettingsActivity.class.getSimpleName();
-
-    //private String maxQuestionsNormalKey;
+public class SettingsActivity extends PreferenceActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,65 +14,9 @@ public class SettingsActivity extends PreferenceActivity implements OnSharedPref
         setContentView(R.layout.settings_activity);
 
         // memo: this is how to reset to defaults
-//		SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
-//		Editor editor = settings.edit();
-//		editor.clear();
-//		editor.commit();
-
-        //maxQuestionsNormalKey = getString(R.string.key_max_questions_normal);
-
-        updatePreferenceGroup(getPreferenceScreen());
-    }
-
-    private void updatePreferenceGroup(PreferenceGroup preferenceGroup) {
-        SharedPreferences sharedPreferences = PreferenceManager
-                .getDefaultSharedPreferences(this);
-        for (int i = 0; i < preferenceGroup.getPreferenceCount(); ++i) {
-            Preference preference = preferenceGroup.getPreference(i);
-            if (preference instanceof PreferenceGroup) {
-                updatePreferenceGroup((PreferenceGroup) preference);
-            } else {
-                updatePreferenceEditor(sharedPreferences, preference);
-            }
-        }
-    }
-
-    private void updatePreferenceEditor(SharedPreferences sharedPreferences,
-                                        String key) {
-        Preference preference = findPreference(key);
-        updatePreferenceEditor(sharedPreferences, preference);
-    }
-
-    private void updatePreferenceEditor(SharedPreferences sharedPreferences,
-                                        Preference preference) {
-        String key = preference.getKey();
-        /*
-        if (key.equals(maxQuestionsNormalKey)) {
-            String value = sharedPreferences.getString(key, null);
-            String format = getString(R.string.summary_max_questions_normal_format);
-            preference.setSummary(String.format(format, value));
-        }
-        */
-    }
-
-    @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
-                                          String key) {
-        Log.d(TAG, "preference changed: " + key);
-        updatePreferenceEditor(sharedPreferences, key);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        getPreferenceScreen().getSharedPreferences()
-                .registerOnSharedPreferenceChangeListener(this);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        getPreferenceScreen().getSharedPreferences()
-                .unregisterOnSharedPreferenceChangeListener(this);
+		//SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
+		//Editor editor = settings.edit();
+		//editor.clear();
+		//editor.commit();
     }
 }
